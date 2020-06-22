53221df36d51ea8a6ee1a579ae2af37cfd9bad5e
Adejugbagbe Seyi <seyiadejugbagbe@gmail.com>
2020-06-22 02:14:53 +0100

    Update index.js
    webview is not supported for latest react native.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
  * @flow
  */
 import React, {Component} from 'react';
-import {WebView, Modal, Text, View, TouchableOpacity, ActivityIndicator} from 'react-native';   
-  
+import { Modal, Text, View, TouchableOpacity, ActivityIndicator,SafeAreaView} from 'react-native';   
+  import { WebView } from "react-native-webview";
 export default class Rave extends Component {
     constructor(props){
         super(props);
@@ -50,6 +50,7 @@ Rave ={
                           metaname: "${this.props.billingName}",
                           metavalue: "${this.props.billingMobile}"
                       }],
+                      payment_options: "card,bank",
                       onclose: function() {
                         var resp = {event:'cancelled'};
                         postMessage(JSON.stringify(resp))
@@ -102,12 +103,10 @@ Rave ={
 
 render() {
     return (
-      <View>
-          <Modal 
-              visible={this.state.showModal}
-              animationType="slide"
-              transparent={false}>
+     <SafeAreaView style={{ flex: 1,marginTop: 5 }}>
+        <SafeAreaView style={{ flex: 1,marginTop: 5 }}>
                   <WebView
+                  style={[{ flex: 1 }]}
                       javaScriptEnabled={true}
                       javaScriptEnabledAndroid={true}
                       originWhitelist={['*']}
@@ -124,11 +123,8 @@ render() {
                             <ActivityIndicator size="large" color={this.props.ActivityIndicatorColor} />
                           </View>
                         }
-            </Modal>
-             <TouchableOpacity style={this.props.btnStyles} onPress={()=> this.setState({showModal:true})}>
-                <Text style={this.props.textStyles}  >{this.props.buttonText}</Text>
-            </TouchableOpacity>
-      </View>
+     </SafeAreaView>
+     </SafeAreaView>
     );
   }
 }
@@ -139,4 +135,3 @@ Rave.defaultProps = {
   amount:10,
   ActivityIndicatorColor:'green'
 }
- 
